ac09c6c98703de34894afbcccbdab4b72258726b
Ashley Whamond <ashleywhamond@gmail.com>
2022-04-16 22:47:29 +1000

    Successfully reading data

--- src/Webhooks.ino
+++ src/Webhooks.ino
@@ -27,27 +27,14 @@ void loop()
   float h = dht.getHumidity();
   // Read temperature as Celsius
   float t = dht.getTempCelcius();
-  // Read temperature as Farenheit
-  float f = dht.getTempFarenheit();
+  
 
   // Check if any reads failed
-  if (isnan(h) || isnan(t) || isnan(f))
+  if (isnan(h) || isnan(t))
   {
-    Serial.println("Failed to read from DHT11 sensor!");
     Particle.publish("Failed to read from DHT11 sensor!");
     return;
   }
-
-  Serial.print("Humid: ");
-  Serial.print(h);
-  Serial.println("%  ");
-  Serial.print("Temp: ");
-  Serial.print(t);
-  Serial.println("*C ");
-  Serial.print("Temp: ");
-  Serial.print(f);
-  Serial.println();
-  Serial.println();
 
   Particle.publish("Humidity:", String(h));
   Particle.publish("Temp:", String(t));
